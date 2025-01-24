213991ce5c9c972715ec89dc0825e35b2735e489
GIRISHRV <girish29052005@gmail.com>
2025-01-25 02:02:01 +0530

    Refactor UI layout and styling for improved user experience; update access token in cache

--- static/main.js
+++ static/main.js
@@ -1,104 +1,44 @@
-document.getElementById('file-input').addEventListener('change', function() {
-    const fileInput = document.getElementById('file-input');
-    const fileName = document.getElementById('file-name');
-    const recommendButton = document.getElementById('recommend-button');
-    const playlistLink = document.getElementById('playlist-link');
-    const errorMessage = document.getElementById('error-message');
+document.addEventListener('DOMContentLoaded', function() {
+    const playlistLinkInput = document.querySelector('.playlist-link');
+    const recommendButton = document.querySelector('.recommend-button');
+    const generateAgainButton = document.getElementById('generate-again-button');
+    const spinner = document.querySelector('.spinner');
+    const recommendationsContainer = document.querySelector('.recommendations-container');
+    const errorMessage = document.createElement('p');
+    errorMessage.style.color = 'red';
+    recommendationsContainer.appendChild(errorMessage);
 
-    if (fileInput.files.length > 0) {
-        const file = fileInput.files[0];
-        const fileExtension = file.name.split('.').pop().toLowerCase();
-
-        if (fileExtension === 'txt') {
-            fileName.textContent = `Selected file: ${file.name}`;
+    playlistLinkInput.addEventListener('input', function() {
+        if (playlistLinkInput.value.trim() !== '') {
             recommendButton.disabled = false;
-            playlistLink.value = ''; // Clear playlist link if file is selected
             errorMessage.textContent = ''; // Clear error message
         } else {
-            fileName.textContent = '';
             recommendButton.disabled = true;
-            errorMessage.textContent = 'Invalid file type. Please select a .txt file.';
+            errorMessage.textContent = ''; // Clear error message
         }
-    } else {
-        fileName.textContent = '';
+    });
+
+    recommendButton.addEventListener('click', function() {
+        const link = playlistLinkInput.value.trim();
+        if (link) {
+            fetchRecommendations(link);
+        } else {
+            errorMessage.textContent = 'Please enter a playlist link.';
+        }
+    });
+
+    generateAgainButton.addEventListener('click', function() {
+        location.reload();
+    });
+
+    function fetchRecommendations(link) {
+        const baseUrl = window.location.origin;
+
+        // Disable inputs and show spinner
         recommendButton.disabled = true;
-    }
-});
+        playlistLinkInput.disabled = true;
+        spinner.classList.remove('d-none');
 
-document.getElementById('playlist-link').addEventListener('input', function() {
-    const playlistLink = document.getElementById('playlist-link');
-    const recommendButton = document.getElementById('recommend-button');
-    const fileInput = document.getElementById('file-input');
-    const fileName = document.getElementById('file-name');
-    const errorMessage = document.getElementById('error-message');
-
-    if (playlistLink.value.trim() !== '') {
-        recommendButton.disabled = false;
-        fileInput.value = ''; // Clear file input if playlist link is provided
-        fileName.textContent = ''; // Clear file name if playlist link is provided
-        errorMessage.textContent = ''; // Clear error message
-
-        // Fetch and display playlist name if the URL is valid
-        fetchPlaylistName(playlistLink.value.trim());
-    } else {
-        recommendButton.disabled = true;
-        fileName.textContent = ''; // Clear file name if playlist link is cleared
-        errorMessage.textContent = ''; // Clear error message
-    }
-});
-
-document.getElementById('recommend-button').addEventListener('click', function() {
-    const fileInput = document.getElementById('file-input');
-    const fileInputLabel = document.querySelector('.file-input-label');
-    const recommendButton = document.getElementById('recommend-button');
-    const spinner = document.getElementById('spinner');
-    const generateAgainButton = document.getElementById('generate-again-button');
-    const fileName = document.getElementById('file-name');
-    const playlistLink = document.getElementById('playlist-link');
-    const errorMessage = document.getElementById('error-message');
-    const recommendationsDiv = document.getElementById('recommendations');
-    const file = fileInput.files[0];
-    const link = playlistLink.value.trim();
-
-    // Clear previous error message
-    errorMessage.textContent = '';
-
-    // Disable buttons and inputs
-    fileInput.disabled = true;
-    fileInputLabel.style.backgroundColor = '#555';
-    fileInputLabel.style.cursor = 'not-allowed';
-    recommendButton.disabled = true;
-    playlistLink.disabled = true;
-
-    // Show spinner
-    spinner.style.display = 'flex';
-
-    // Clear previous recommendations
-    recommendationsDiv.innerHTML = '';
-
-    const baseUrl = window.location.origin;
-
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function(event) {
-            const fileContent = event.target.result;
-            const tracks = parseFileContent(fileContent);
-            getRecommendations(tracks, baseUrl)
-                .then(recommendations => {
-                    displayRecommendations(recommendations);
-                    // Hide spinner
-                    spinner.style.display = 'none';
-                    // Show generate again button
-                    generateAgainButton.style.display = 'inline-block';
-                })
-                .catch(error => {
-                    errorMessage.textContent = 'Failed to get recommendations. Please try again.';
-                    // Hide spinner
-                    spinner.style.display = 'none';
-                });
-        };
-        reader.readAsText(file);
-    } else if (link) {
         fetch(`${baseUrl}/playlist-details`, {
             method: 'POST',
             headers: {
@@ -114,109 +54,62 @@ document.getElementById('recommend-button').addEventListener('click', function()
         })
         .then(data => {
             const { playlistName, tracks } = data;
-            fileName.textContent = `Selected playlist: ${playlistName}`;
             getRecommendations(tracks, baseUrl)
                 .then(recommendations => {
                     displayRecommendations(recommendations);
-                    // Hide spinner
-                    spinner.style.display = 'none';
-                    // Show generate again button
+                    spinner.classList.add('d-none');
                     generateAgainButton.style.display = 'inline-block';
                 })
                 .catch(error => {
                     errorMessage.textContent = 'Failed to get recommendations. Please try again.';
-                    // Hide spinner
-                    spinner.style.display = 'none';
+                    spinner.classList.add('d-none');
                 });
         })
         .catch(error => {
             errorMessage.textContent = 'Failed to fetch playlist details. Please ensure the playlist is public and accessible.';
-            // Hide spinner
-            spinner.style.display = 'none';
+            spinner.classList.add('d-none');
         });
-    } else {
-        errorMessage.textContent = 'Please upload a file or enter a playlist link.';
-        // Hide spinner
-        spinner.style.display = 'none';
+    }
+
+    function getRecommendations(tracks, baseUrl) {
+        return fetch(`${baseUrl}/recommendations`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ tracks: tracks })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        });
+    }
+
+    function displayRecommendations(recommendations) {
+        recommendationsContainer.innerHTML = ''; // Clear previous recommendations
+
+        if (Array.isArray(recommendations) && recommendations.length > 0) {
+            recommendations.forEach(rec => {
+                const imageUrl = rec.image ? rec.image : 'static/missing.jpg';
+                const recElement = document.createElement('div');
+                recElement.className = 'col';
+                recElement.innerHTML = `
+                    <div class="card text-white bg-success mb-3" style="width: 12rem;">
+                        <img class="card-img-top" src="${imageUrl}" alt="${rec.track}" style="height: 150px; object-fit: cover;">
+                        <div class="card-body">
+                            <h5 class="card-title">${rec.track}</h5>
+                            <p class="card-text">${rec.artist}</p>
+                            <p class="card-text">${rec.release_date}</p>
+                            <a href="${rec.spotifyUrl}" class="btn btn-primary">Spotify</a>
+                        </div>
+                    </div>
+                `;
+                recommendationsContainer.appendChild(recElement);
+            });
+        } else {
+            recommendationsContainer.innerHTML = '<p>No recommendations available.</p>';
+        }
     }
 });
-
-document.getElementById('generate-again-button').addEventListener('click', function() {
-    location.reload();
-});
-
-function parseFileContent(content) {
-    // Example function to parse file content into an array of tracks
-    // Assuming each line in the file is in the format "artist - track"
-    return content.split('\n').map(line => {
-        const [artist, track] = line.split(' - ');
-        return { artist, track };
-    });
-}
-
-function getRecommendations(tracks, baseUrl) {
-    return fetch(`${baseUrl}/recommendations`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ tracks: tracks })
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    });
-}
-
-function fetchPlaylistName(playlistLink) {
-    const baseUrl = window.location.origin;
-    fetch(`${baseUrl}/playlist-details`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ playlistLink: playlistLink })
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        const { playlistName } = data;
-        const fileName = document.getElementById('file-name');
-        fileName.textContent = `Selected playlist: ${playlistName}`;
-    })
-    .catch(error => {
-        const fileName = document.getElementById('file-name');
-        fileName.textContent = ''; // Clear file name if playlist link is invalid
-    });
-}
-
-function displayRecommendations(recommendations) {
-    const recommendationsDiv = document.getElementById('recommendations');
-    recommendationsDiv.innerHTML = ''; // Clear previous recommendations
-
-    if (Array.isArray(recommendations) && recommendations.length > 0) {
-        recommendations.forEach(rec => {
-            //console.log('Recommendation:', rec); // Debugging statement
-            const imageUrl = rec.image ? rec.image : 'static/missing.jpg';
-            const recElement = document.createElement('div');
-            recElement.className = 'item';
-            recElement.innerHTML = `
-                <a href="${rec.spotifyUrl}" target="_blank" style="text-decoration: none; color: inherit;">
-                    <img src="${imageUrl}" alt="${rec.track}">
-                    <h4>${rec.track}</h4>
-                    <p>${rec.artist}</p>
-                    <p>${rec.release_date}</p>
-                </a>
-            `;
-            recommendationsDiv.appendChild(recElement);
-        });
-    } else {
-        recommendationsDiv.innerHTML = '<p>No recommendations available.</p>';
-    }
-}
